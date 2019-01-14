4e3c929a3ac0cb845ecd2452518f6e04cdac19a8
Jakub Juszczak <jakub.juszczak@microsoft.wsei.edu.pl>
2019-01-14 17:52:22 +0100

    OrderedCar table update + View1

--- Projekt/Projekt/dbo/Tables/OrderedCar.sql
+++ Projekt/Projekt/dbo/Tables/OrderedCar.sql
@@ -1,11 +1,11 @@
 ﻿CREATE TABLE [dbo].[OrderedCar] (
-    [OrderedCarID] INT        IDENTITY (1, 1) NOT NULL,
-    [ModelID]      INT        NULL,
-    [OrderID]      INT        NULL,
-    [FinalPrice]   SMALLMONEY NOT NULL,
+    [OrderedCarID] INT IDENTITY (1, 1) NOT NULL,
+    [ModelID]      INT NULL,
+    [OrderID]      INT NULL,
     PRIMARY KEY CLUSTERED ([OrderedCarID] ASC),
-    CHECK ([FinalPrice]>(0)),
     FOREIGN KEY ([ModelID]) REFERENCES [dbo].[Model] ([ModelID]) ON DELETE SET NULL ON UPDATE SET NULL,
     FOREIGN KEY ([OrderID]) REFERENCES [dbo].[Order] ([OrderID]) ON DELETE SET NULL ON UPDATE SET NULL
 );
+
+
 
---
+++ Projekt/Projekt/rapOp/Views/View1.sql
@@ -0,0 +1,14 @@
+﻿CREATE VIEW [rapOp].[View1] AS
+
+
+SELECT
+[ma].[Name],
+COUNT([oc].ModelID) AS OrderNumber,
+SUM([pr].Price) AS SumOfMoney
+FROM [Manufacturer] AS ma (READUNCOMMITTED) INNER JOIN
+[Model] AS mo (READUNCOMMITTED) ON [ma].[ManufacturerID] = [mo].[ManufacturerID] INNER JOIN
+[OrderedCar] AS oc (READUNCOMMITTED) ON [oc].[ModelID] = [mo].[ModelID] INNER JOIN
+[Price] AS pr (READUNCOMMITTED) ON [pr].ModelID = [mo].ModelID
+GROUP BY
+[ma].[Name]
+;
