fdaa77a3cbe206749a7991d8f20304ed6ca7b3d0
Jakub Juszczak <jakub.juszczak@microsoft.wsei.edu.pl>
2019-01-14 18:33:49 +0100

    OrderHistory Table update

--- Projekt/Projekt/SRVC/Tables/OrderHistory.sql
+++ Projekt/Projekt/SRVC/Tables/OrderHistory.sql
@@ -7,8 +7,11 @@
     PRIMARY KEY CLUSTERED ([OrderHistoryID] ASC),
     FOREIGN KEY ([OrderID]) REFERENCES [SRVC].[Order] ([OrderID]),
     FOREIGN KEY ([Status]) REFERENCES [SRVC].[OrderStatus] ([Name]),
-    FOREIGN KEY ([UpdatedByEmployee]) REFERENCES [dbo].[Employee] ([EmployeeID])
+    FOREIGN KEY ([UpdatedByEmployee]) REFERENCES [dbo].[Employee] ([EmployeeID]),
+    UNIQUE NONCLUSTERED ([OrderID] ASC, [Status] ASC)
 );
+
+
 
 
 
