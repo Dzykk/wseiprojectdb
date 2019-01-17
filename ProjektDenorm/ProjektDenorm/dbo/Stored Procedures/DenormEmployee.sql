56dc40bccfe329c7183e1b1b9e4fd054ba412886
Jakub Juszczak <jakub.juszczak@microsoft.wsei.edu.pl>
2019-01-17 11:34:19 +0100

    Denormalization Procedure - Employee commit

---
+++ ProjektDenorm/ProjektDenorm/dbo/Stored Procedures/DenormEmployee.sql
@@ -0,0 +1,46 @@
+﻿CREATE PROCEDURE DenormEmployee
+AS
+BEGIN --begin proc
+
+WITH temptable AS
+(
+SELECT
+nem.EmployeeID AS EmployeeID,
+nem.[Name],
+[Surname],
+Gender,
+Email,
+PhoneNumber,
+PESEL,
+HireDate,
+njo.[Name] AS Job,
+nej.DateFrom AS JobDateFrom,
+nej.DateTo AS JobDateTo,
+ntm.[Name] AS Team,
+nwa.Amount AS WageAmount
+FROM
+Projekt.dbo.Employee AS nem INNER JOIN
+(Projekt.HR.EmployeeJob AS nej  INNER JOIN
+Projekt.HR.Job AS njo ON nej.JobID = njo.JobID) ON nem.EmployeeID = nej.EmployeeID INNER JOIN
+(Projekt.HR.EmployeeTeam AS net INNER JOIN Projekt.HR.Team AS ntm ON net.TeamID = ntm.TeamID) ON nem.EmployeeID = net.EmployeeID INNER JOIN
+Projekt.HR.Wage AS nwa ON nwa.EmployeeID = nem.EmployeeID
+)
+
+MERGE dbo.Employee AS tar
+USING temptable AS src
+ON tar.EmployeeID = src.EmployeeID
+WHEN MATCHED THEN
+UPDATE SET
+tar.Surname = src.Surname,
+tar.Email = src.Email,
+tar.PhoneNumber = src.PhoneNumber,
+tar.Job = src.Job,
+tar.JobDateFrom = src.JobDateFrom,
+tar.JobDateTo = src.JobDateTo,
+tar.Team = src.Team,
+tar.WageAmount = src.WageAmount
+WHEN NOT MATCHED THEN
+INSERT (EmployeeID, [Name], Surname, Gender, Email, PhoneNumber, PESEL, HireDate, Job, JobDateFrom, JobDateTo, Team, WageAmount)
+VALUES
+(src.EmployeeID, src.[Name], src.Surname, src.Gender, src.Email, src.PhoneNumber, src.PESEL, src.HireDate, src.Job, src.JobDateFrom, src.JobDateTo, src.Team, src.WageAmount);
+END --end proc
--- ProjektDenorm/ProjektDenorm/dbo/Tables/Employee.sql
+++ ProjektDenorm/ProjektDenorm/dbo/Tables/Employee.sql
@@ -1,23 +1,23 @@
 ﻿CREATE TABLE [dbo].[Employee] (
-    [EmployeeID]       INT           NOT NULL,
-    [Name]             VARCHAR (20)  NOT NULL,
-    [Surname]          VARCHAR (25)  NOT NULL,
-    [Gender]           VARCHAR (6)   NULL,
-    [Email]            VARCHAR (50)  NULL,
-    [PhoneNumber]      VARCHAR (20)  NULL,
-    [PESEL]            CHAR (11)     NOT NULL,
-    [HireDate]         SMALLDATETIME NOT NULL,
-    [Job]              VARCHAR (50)  NOT NULL,
-    [JobDateFrom]      SMALLDATETIME NOT NULL,
-    [JobDateTo]        SMALLDATETIME NOT NULL,
-    [Team]             VARCHAR (60)  NOT NULL,
-    [WageAmount]       SMALLMONEY    NULL,
-    [PayoutPaymentSum] SMALLMONEY    NULL,
-    [BonusPaymentSum]  SMALLMONEY    NULL,
+    [EmployeeID]  INT           NOT NULL,
+    [Name]        VARCHAR (20)  NOT NULL,
+    [Surname]     VARCHAR (25)  NOT NULL,
+    [Gender]      VARCHAR (6)   NULL,
+    [Email]       VARCHAR (50)  NULL,
+    [PhoneNumber] VARCHAR (20)  NULL,
+    [PESEL]       CHAR (11)     NOT NULL,
+    [HireDate]    SMALLDATETIME NOT NULL,
+    [Job]         VARCHAR (50)  NOT NULL,
+    [JobDateFrom] SMALLDATETIME NOT NULL,
+    [JobDateTo]   SMALLDATETIME NOT NULL,
+    [Team]        VARCHAR (60)  NOT NULL,
+    [WageAmount]  SMALLMONEY    NULL,
     PRIMARY KEY CLUSTERED ([EmployeeID] ASC),
     CHECK ([Gender] like '%male'),
     UNIQUE NONCLUSTERED ([Email] ASC),
     UNIQUE NONCLUSTERED ([PESEL] ASC),
     UNIQUE NONCLUSTERED ([PhoneNumber] ASC)
 );
+
+
 
