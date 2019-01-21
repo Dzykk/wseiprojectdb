7af4eba39214408e7e8f4358a1f980aa256a6568
Jakub Juszczak <jakub.juszczak@microsoft.wsei.edu.pl>
2019-01-21 21:14:05 +0100

    Third raporting procedure commit + Order check constraint update

--- Projekt/Projekt/dbo/Tables/Order.sql
+++ Projekt/Projekt/dbo/Tables/Order.sql
@@ -8,10 +8,12 @@
     [Completed]    BIT           DEFAULT ('0') NOT NULL,
     [Delivered]    BIT           DEFAULT ('0') NOT NULL,
     PRIMARY KEY CLUSTERED ([OrderID] ASC),
-    CONSTRAINT [CK__Order__FullPrice__68487DD7] CHECK ([FullPrice]>(0)),
+    CONSTRAINT [CK__Order__FullPrice__68487DD7] CHECK ([FullPrice]>=(0)),
     FOREIGN KEY ([ClientID]) REFERENCES [dbo].[Client] ([ClientID]) ON DELETE SET NULL ON UPDATE SET NULL,
     FOREIGN KEY ([EmployeeID]) REFERENCES [dbo].[Employee] ([EmployeeID]) ON DELETE SET NULL ON UPDATE SET NULL
 );
+
+
 
 
 
---
+++ ProjektDenorm/ProjektDenorm/rapAn/Stored Procedures/Raport3.sql
@@ -0,0 +1,48 @@
+﻿
+CREATE   PROCEDURE rapAn.Raport3 @yr int
+AS
+CREATE TABLE #Temp
+(
+[MonthName] varchar(15),
+[MonthNumber] int
+)
+INSERT INTO #Temp
+SELECT DATENAME(MONTH, DATEADD(MM, s.number, CONVERT(DATETIME, 0))) AS [MonthName], 
+MONTH(DATEADD(MM, s.number, CONVERT(DATETIME, 0))) AS [MonthNumber] 
+FROM master.dbo.spt_values s 
+WHERE [type] = 'P' AND s.number BETWEEN 0 AND 11
+ORDER BY 2
+
+DECLARE @cols AS varchar(MAX) = ''
+DECLARE @sql AS varchar(MAX) = ''
+SELECT @cols = @cols + QUOTENAME([MonthName]) + ','
+FROM 
+#Temp
+
+SET @cols = substring(@cols, 0, len(@cols))
+DECLARE @year varchar(15) = CAST(@yr AS varchar(15))
+BEGIN
+SET @sql =
+'SELECT *
+FROM
+(
+SELECT 
+cm.ManufacturerName,
+cm.ModelName,
+DATENAME(month, od.OrderDate) AS OrderDate,
+od.FinalPrice
+FROM
+[Order] AS od INNER JOIN
+CarModel AS cm ON od.CarModelID = cm.CarModelID
+WHERE YEAR(od.OrderDate) = ' + @year + ' 
+) AS src
+PIVOT
+( SUM(FinalPrice) FOR OrderDate IN (' + @cols + ')) AS piv
+'
+EXEC(@sql)
+
+IF(OBJECT_ID('tempdb..#temp') IS NOT NULL)
+BEGIN
+DROP TABLE #Temp
+END
+END
